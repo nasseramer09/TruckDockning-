eb603a4ac42c0868290625c9edc7ea22dab48627
Nasser Amer <nasser_amer09@yahoo.com>
2023-12-21 14:34:20 +0100

    made another switch instade of if in station.java file

--- src/Stationen.java
+++ src/Stationen.java
@@ -5,19 +5,14 @@ import java.util.Scanner;
 public class Stationen{
     private  List<Vehicle> registreringAvLastBilar;
 
-
     public Stationen(List<Vehicle> registreringAvLastBilar){
         this.registreringAvLastBilar = registreringAvLastBilar;
-
     }
-
     public void dialogMedKunden(){
 
     Scanner input= new Scanner(System.in);
     List<Vehicle> registreringAvSkapBilar= new ArrayList<>();
     KajStatusKontroller kajStatusKontroller= new KajStatusKontroller();
-
-
 
     while (true){
 
@@ -30,54 +25,56 @@ public class Stationen{
 
         int userInput= input.nextInt();
         switch (userInput){
-            case 1: if (registreringAvSkapBilar.isEmpty()){
-                System.out.println("Just nu finns det inga bilar parkerad ");
-            }
-            else{
-                for (Vehicle parkeradeVehclar: registreringAvSkapBilar){
-                    String kaj = parkeradeVehclar.getKajar();
-                    System.out.println(" Här är de last bilar som är parkerade just nu " + parkeradeVehclar.getVehicleTyp()+" "
-                            + parkeradeVehclar.getVehicleWeight() + " Ton i kaj " + kaj  );
+            case 1:
+                if (registreringAvSkapBilar.isEmpty()){
+                    System.out.println("Just nu finns det inga bilar parkerad ");
                 }
-
-            }
+                else{
+                    for (Vehicle parkeradeVehclar: registreringAvSkapBilar){
+                        String kaj = parkeradeVehclar.getKajar();
+                        System.out.println(" Här är de last bilar som är parkerade just nu " + parkeradeVehclar.getVehicleTyp()+" "
+                                + parkeradeVehclar.getVehicleWeight() + " Ton i kaj " + kaj  );
+                    }
+                }
+                break;
             case 2:
-                System.out.println("Registerera en ny lastbil \n Följ instruktionerarna nedan\n Tryck 1 för skåpbil\n Tryck 2 för lätt lastbil\n Tryck 3 för tung lastbil" );
+                System.out.println("Registerera en ny lastbil \n " +
+                        "Följ instruktionerarna nedan\n " +
+                        "Tryck 1 för skåpbil\n " +
+                        "Tryck 2 för lätt lastbil\n " +
+                        "Tryck 3 för tung lastbil" );
 
                 int newInput =input.nextInt();
-                if (newInput==1){
-                    if (registreringAvSkapBilar.size()<5){
+                switch (newInput){
+                    case 1:
+                            if (registreringAvSkapBilar.size()<5){
+                            String vehicleTyp="skåpBil";
+                            SkapBil skapBil = new SkapBil(vehicleTyp,0);
+                            registreringAvSkapBilar.add(skapBil);
+                            kajStatusKontroller.kontrolleraStatusPaKajar(skapBil);
+                        }
+                        else{
+                            System.out.println("Det är fullt upp just nu i Dumpa mera \n Besök Dumpa mest stationen ");
+                        }
+                        break;
+                    case 2:
+                        if (registreringAvSkapBilar.size()<5){
 
-                        String vehicleTyp="skåpBil";
-                        SkapBil skapBil = new SkapBil(vehicleTyp,0);
+                            String vehicleTyp="Lätt Lastbil";
+                            System.out.println("Skriv in hur mycket lätt lastbilen väger i ton ");
+                            double vehicleWeight= input.nextDouble();
+                            LattLastBil lattLastBil = new LattLastBil(vehicleTyp,vehicleWeight);
+                            registreringAvSkapBilar.add(lattLastBil);
+                            kajStatusKontroller.kontrolleraStatusPaKajar(lattLastBil);
 
-                        registreringAvSkapBilar.add(skapBil);
-                        kajStatusKontroller.kontrolleraStatusPaKajar(skapBil);
+                        }
+                        else{
+                            System.out.println("Det är fullt upp just nu i kajarna  \n vänta tills någon av kajen är lediga");
+                        }
+                        break;
 
-                    }
-                    else{
-                        System.out.println("Det är fullt upp just nu i Dumpa mera \n Besök Dumpa mest stationen ");
-                    }
-
-
-                } else if (newInput==2){
-                    if (registreringAvSkapBilar.size()<5){
-
-                        String vehicleTyp="Lätt Lastbil";
-                        System.out.println("Skriv in hur mycket lätt lastbilen väger i ton ");
-                        double vehicleWeight= input.nextDouble();
-                        LattLastBil lattLastBil = new LattLastBil(vehicleTyp,vehicleWeight);
-                        registreringAvSkapBilar.add(lattLastBil);
-                        kajStatusKontroller.kontrolleraStatusPaKajar(lattLastBil);
-
-                    }
-                    else{
-                        System.out.println("Det är fullt upp just nu i kajarna  \n vänta tills någon av kajen är lediga");
-                    }
-
-                }
-                else if(newInput==3){
-                    if (registreringAvSkapBilar.size()<5){
+                    case 3:
+                        if(registreringAvSkapBilar.size()<5){
 
                         String vehicleTyp="Tung Lastbil";
                         System.out.println("Skriv in hur mycket lätt lastbilen väger i ton ");
@@ -90,14 +87,14 @@ public class Stationen{
                     else{
                         System.out.println("Kajarna är upptagna just nu \n vänta tills någon  kaj är ledig");
                     }
-                }
-                else{
-                    System.out.println("Fel inmatning \n Var god och följ instruktionerna nedean");
+                         break;
+                    default:  System.out.println("Fel inmatning \n Var god och följ instruktionerna nedean");
+                         break;
 
                 }
-                break;
+              break;
             case 3: System.out.println("Programet avslutas här\n välkommen åter");
-                break;
+                     break;
         }
 
       }
